ed52a1827892dab31b73b93d4bfe9d1b73fd4087
Mihailob5017 <mihailob5017@gmail.com>
2021-01-06 22:51:31 +0100

    setup a pwa with tailwind css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const App = () => {
   return (
-   <h1>Hello world</h1>
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div>
+        <div class="text-xl font-medium text-black">Welcome to Tailwind!</div>
+        <p class="text-gray-500">I am here to make you stay!</p>
+      </div>
+    </div>
   )
 }
 
